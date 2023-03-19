749be478ea9d94cf3e677c1aba8305ca962904ac
Luis Inacio <lrs.inacio@gmail.com>
2023-03-19 15:05:18 +0000

    first commit

---
+++ DataBindingMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DataBindingMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DataBindingMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DataBindingMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
